55215b3b49d55bb519bd2e8b82651bc88e8c41b3
kseebrinegar <ksee.brinegar@hotmail.com>
2017-11-29 15:24:15 -0800

    first commit

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+//dev.js - dont commit this!!!
+module.exports = {
+	googleClientID: '982638913246-9eu3m07etf8dr33ti9u8egr72fhkh3gv.apps.googleusercontent.com',
+	googleClientSecret: 'WFoHDWU2ZJfArbroadJhE5hm',
+	mongoURI: 'mongodb://caseyBrinegar:React!12@ds117316.mlab.com:17316/casey-emaily-dev',
+	cookieKey: 'asdhfasjdfasljkfjasdfhasdwqwqpppsskasdk'
+};
+
